67434b985451e339a85b93719fc1df0eea18a581
Semyon Golgan <94925577+black-evil-dragon@users.noreply.github.com>
2022-11-12 13:54:01 +0300

    remove server

--- server/src/functions/index.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-export const test = (req: Request, res: Response) => {
-    res.send('Hello')
-}
-
-export const homePage = (req: Request, res: Response) => {
-    res.sendFile(__dirname + '/index.html')
-}
--- server/src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import { Express } from 'express';
-import { homePage } from '../functions'
-
-module.exports = function(app: Express) {
-    app.get('/', homePage)
-}
